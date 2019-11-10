62d7174fce4e6fa70a91eb7e3e718f2f42acfa12
mr_ <njgriffiths@gmail.com>
2019-11-09 22:26:55 -0800

    Updates

---
+++ precache-manifest.dddf1446fd59af4f72f29f2b7a8009b7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "049582893232c0d615e28962b166636b",
+    "url": "/translink-alerts-app/index.html"
+  },
+  {
+    "revision": "5a1ad907434a5553de98",
+    "url": "/translink-alerts-app/static/css/main.04ebba2f.chunk.css"
+  },
+  {
+    "revision": "ec14eb012e5d855c520e",
+    "url": "/translink-alerts-app/static/js/2.9fbf4d89.chunk.js"
+  },
+  {
+    "revision": "5a1ad907434a5553de98",
+    "url": "/translink-alerts-app/static/js/main.f3897ac0.chunk.js"
+  },
+  {
+    "revision": "ffef2b9ba4e67e1372eb",
+    "url": "/translink-alerts-app/static/js/runtime~main.2325472d.js"
+  },
+  {
+    "revision": "6f6753fd733a216b24786a168657540a",
+    "url": "/translink-alerts-app/static/media/BentonSansCond-Bold.6f6753fd.otf"
+  },
+  {
+    "revision": "a6640d8951e3d6c523b2b58b0db9e351",
+    "url": "/translink-alerts-app/static/media/BentonSansCond-BoldItalic.a6640d89.otf"
+  },
+  {
+    "revision": "69a4217c7da7462a2511b1a8ecaefecf",
+    "url": "/translink-alerts-app/static/media/BentonSansCond-RegItalic.69a4217c.otf"
+  },
+  {
+    "revision": "43cd4ac75abf53e257d21366d3322b0e",
+    "url": "/translink-alerts-app/static/media/BentonSansCond-Regular.43cd4ac7.otf"
+  },
+  {
+    "revision": "cdd38a860699d029f95e76f21e51e216",
+    "url": "/translink-alerts-app/static/media/Shift-Bold.cdd38a86.otf"
+  },
+  {
+    "revision": "5c3edbfdeedc59bb35fac3bf1a461926",
+    "url": "/translink-alerts-app/static/media/Shift-BoldItalic.5c3edbfd.otf"
+  }
+]);
